5ab1abbf0dc99d4e0e532d353becb6bd3fd6bc46
Monshouwer <rene.monshouwer@radboudumc.nl>
2023-11-21 09:35:02 +0100

    added .nii.gz files

---
+++ convert_plastimatch_nii.bat
@@ -0,0 +1,2 @@
+plastimatch convert --input "dicom_1234567/1.3.46.670589.13.880152.20221021125433.862061_0003_000000_16663496790047.dcm" --output-prefix plastimatch_1234567 --prefix-format "nii.gz" --referenced-ct dicom_1234567
+
